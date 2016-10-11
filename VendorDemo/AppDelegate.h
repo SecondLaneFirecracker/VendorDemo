14ed4071f36f624927c5e08ec34e8545feb3cfe7
Mark Song <marksong@augmentum.com.cn>
2016-10-11 17:40:18 +0800

    Add AFNetworking

---
+++ VendorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VendorDemo
+//
+//  Created by marksong on 10/11/16.
+//  Copyright © 2016 S.R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VendorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VendorDemo
+//
+//  Created by marksong on 10/11/16.
+//  Copyright © 2016 S.R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
